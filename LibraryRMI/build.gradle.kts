eb4eeae627e787c0fc10819fcf1779ec0af6a3dc
joelao <joel.stephan.email@gmail.com>
2025-02-18 09:44:01 +0100

    Aggiunta della configurazione per l'applicazione e supporto per l'esecuzione del server e del client RMI

--- LibraryRMI/build.gradle.kts
+++ LibraryRMI/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-    id("java")
+    java
+    application
 }
 
-group = "joel.stephan"
+group = "library"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,10 +11,48 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+}
+
+tasks.compileJava {
+    options.release.set(11)
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("server.LibraryServer")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+    modularity.inferModulePath.set(true)
+}
+
+tasks.register<JavaExec>("runServer") {
+    group = "application"
+    mainClass.set("server.LibraryServer")
+    classpath = sourceSets["main"].runtimeClasspath
+    jvmArgs = listOf(
+        "-Djava.security.policy=security.policy",
+        "-Djava.rmi.server.codebase=file:${projectDir}/build/classes/java/main/"
+    )
+}
+
+tasks.register<JavaExec>("runClient") {
+    group = "application"
+    mainClass.set("client.LibraryClient")
+    classpath = sourceSets["main"].runtimeClasspath
+    jvmArgs = listOf(
+        "-Djava.security.policy=security.policy"
+    )
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "server.LibraryServer"
+    }
 }
