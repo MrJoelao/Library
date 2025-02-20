1f36c95c6864daf7fd41cdee148ec215ee812bbe
Joel Stephan <Joel.stephan.email@gmail.com>
2025-02-20 20:32:03 +0100

    Aggiunta della configurazione per il server RMI e il client web, inclusi file properties e gestione dello stato del server tramite interfaccia web

--- LibraryRMI/web-client/src/main/java/com/library/web/LibraryWebApplication.java
+++ LibraryRMI/web-client/src/main/java/com/library/web/LibraryWebApplication.java
@@ -3,6 +3,7 @@ package com.library.web;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,10 +15,16 @@ import com.library.rmi.service.LibraryService;
 @SpringBootApplication
 public class LibraryWebApplication {
 
+    @Value("${rmi.registry.host}")
+    private String rmiHost;
+
+    @Value("${rmi.registry.port}")
+    private int rmiPort;
+
     @Bean
     public LibraryService libraryService() throws Exception {
-        System.setProperty("java.rmi.server.hostname", "localhost");
-        Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+        System.setProperty("java.rmi.server.hostname", rmiHost);
+        Registry registry = LocateRegistry.getRegistry(rmiHost, rmiPort);
         return (LibraryService) registry.lookup("LibraryService");
     }
 
