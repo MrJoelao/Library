34cd756dff8ca943f0f216a1bd57704dca86ee1c
joelao <joel.stephan.email@gmail.com>
2025-03-05 10:44:13 +0100

    Aggiunta della classe User con metodi getter, setter e implementazione di equals e hashCode

---
+++ LibraryRMI/rmi-server/src/main/java/com/library/rmi/model/User.java
@@ -0,0 +1,46 @@
+package com.library.rmi.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private String username;
+    private String password;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return username.hashCode();
+    }
+}
